16fe577c923ad96f709d5d37029c72bc3a6d8a36
Baris34 <bariskaya5734@gmail.com>
2024-07-29 14:13:48 +0300

    MechanicPush

---
+++ OUABootcamp/Assets/BarisDev/Scripts/Datas/PlayerData.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "GameData",menuName = "PlayerData")]
+public class PlayerData : ScriptableObject
+{
+    [Header("Player Stats")]
+    [SerializeField] private int playerHealth;
+    [SerializeField] private int playerDamage;
+    [SerializeField] private int playerMaxHealth;
+
+    public int PlayerHealth
+    {
+        get => playerHealth;
+        set
+        {
+            if (playerHealth<=0)
+            {
+                playerHealth = 0;
+            }
+            else
+            {
+                playerHealth = value;
+            }
+        }
+    }
+}
