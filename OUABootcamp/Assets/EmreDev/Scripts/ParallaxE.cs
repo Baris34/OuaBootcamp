90abbd28b3922dccca0ff0fa21df733803f81d8d
Mehmet Emre <121262873+Mehmet5371@users.noreply.github.com>
2024-08-01 22:54:42 +0300

    Add files via upload

---
+++ OUABootcamp/Assets/EmreDev/Scripts/ParallaxE.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxE : MonoBehaviour
+{
+    public GameObject _cam;
+    [SerializeField] private float _speed;
+    void Update()
+    {
+        transform.Translate(-1 * _speed * Time.deltaTime, 0.0f, 0.0f);
+
+        if (_cam.transform.position.x >= transform.position.x + 18f)
+        {
+            transform.position = new Vector2(
+                _cam.transform.position.x + 18f,
+                transform.position.y
+            );
+        }
+    }
+}
